8902ad051fcb0063ff37c22cc614d3b22622d3ae
Cruz <racruz7@me.com>
2026-02-01 21:08:34 -0600

    fix: eslint fixes

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.jest
+      }
+    },
+    rules: {
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_', caughtErrors: 'none' }],
+      'no-console': 'off',
+      'no-process-exit': 'off'
+    }
+  }
+];
